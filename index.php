15bbd3c8724ce09212d32fa0c125fa1e5e44ee57
abkn9977 <abkn9977@gmail.com>
2020-12-13 16:37:37 +0530

    Stripe php library added and some fields are added in ui

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paypage</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h2 class="mt-4 text-center">Intro to React course [$30]</h2>
+        <form action="charge.php" method="post" id="payment-form">
+            <div class="form-row">
+                <input type="text" name="first_name" class="form-control my-3 StripeElement StripeElement--empty" placeholder="First name">
+                <input type="text" name="last_name" class="form-control my-3 StripeElement StripeElement--empty" placeholder="Last name">
+                <input type="email" name="email_address" class="form-control my-3 StripeElement StripeElement--empty" placeholder="Email address">
+                <div id="card-element" class="form-group">
+                    <!-- A Stripe Element will be inserted here. -->
+                </div>
+
+                <!-- Used to display form errors. -->
+                <div id="card-errors" role="alert"></div>
+            </div>
+            <button>Submit Payment</button>
+        </form>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="js/charge.js"></script>
+</body>
+
+</html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '09a50d3e528dbc098db31c2ed6f8de90d8790695',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '09a50d3e528dbc098db31c2ed6f8de90d8790695',
+    ),
+    'stripe/stripe-php' => 
+    array (
+      'pretty_version' => 'v7.67.0',
+      'version' => '7.67.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '935d2c67912007f6d17b6c08a62050252c509129',
+    ),
+  ),
+);
